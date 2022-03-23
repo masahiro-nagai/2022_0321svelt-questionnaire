3901496e3f0a83c350746d5cf91f8860d7229ab1
ハマヤ永井 <hamayanagai@MacBook-Pro-2.local>
2022-03-24 00:23:52 +0900

    Create Form

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import FeedbackForm from "./components/FeedbackForm.svelte";
+	import FeedbackList from "./components/FeedbackList.svelte";
+	import FeedbackStats from "./components/FeedbackStats.svelte";
+
+	let feedback = [
+		{
+			id:1,
+			rating: 10,
+			text: 'testtesttest'
+		},
+		{
+			id:2,
+			rating: 9,
+			text: 'テストストテスト'
+		},
+		{
+			id:3,
+			rating: 8,
+			text: 'TESTTESTTEST'
+		}
+	]
+
+	$: count = feedback.length
+	$: average = feedback.reduce((a,{rating})=> a + rating,0) / feedback.length
+	const deleteFeedback = (e) => {
+		const itemId = e.detail
+		feedback = feedback.filter((item) => item.id != itemId)
+	}
+
+</script>
+
+<main class="container">
+	<FeedbackForm />
+	<FeedbackStats {count} {average} />
+	<FeedbackList {feedback} on:delete-feedback ={deleteFeedback}/>
+	
+</main>
---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,65 @@
+<script>
+    import Card from "./Card.svelte";
+    import Button from "./Button.svelte";
+
+    let text = ''
+    let btnDisable = true
+    let min = 10
+    let message 
+    const handleInput = () => {
+        if(text.trim().length <= min){
+        message = `{min}文字以上入力してください`
+        btnDisable = true
+        }else {
+            message = null
+            btnDisable = false
+        }
+    }
+
+</script>
+<Card>
+    <header>
+        <h2>アンケートをお願いします</h2>
+    </header>
+<form>
+    <!-- Rating select -->
+    <div class="input-group">
+        <input type="text" on:input ={handleInput} bind:value = {text} placeholder="ご意見はこちらに入力してください">
+        <Button disabled={btnDisable} type = "submit">送信</Button>
+    </div>
+    {#if message}
+    <div class="message">
+        {message}
+    </div>
+    {/if}
+</form>
+</Card>
+
+<style>
+    header {
+      max-width: 400px;
+      margin: auto;
+    }
+    header h2 {
+      font-size: 22px;
+      font-weight: 600;
+      text-align: center;
+    }
+    .input-group {
+      display: flex;
+      flex-direction: row;
+      border: 1px solid #ccc;
+      padding: 8px 10px;
+      border-radius: 8px;
+      margin-top: 15px;
+    }
+    input {
+      flex-grow: 2;
+      border: none;
+      font-size: 16px;
+    }
+    input:focus {
+      outline: none;
+    }
+    
+  </style>
